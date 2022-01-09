23e64cf57765ed584f85a4650e428b3e3e632e49
GeorgeRosianu <georgerosianu12@gmail.com>
2022-01-09 21:14:51 +0200

    Clean-up

--- food-delivery-app/src/app/components/browse/browse.component.ts
+++ food-delivery-app/src/app/components/browse/browse.component.ts
@@ -1,39 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 
-import { User } from 'src/app/core/models/user';
-import { UserService } from 'src/app/core/services/user.service';
-import { AuthenticationService } from 'src/app/core/services/authentication.service';
-
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
   styleUrls: ['./browse.component.less']
 })
 export class BrowseComponent implements OnInit {
-  currentUser: User;
-  users: User[] = [];
 
   constructor(
-    private authenticationService: AuthenticationService,
-    private userService: UserService
-  ) {
-    this.currentUser = this.authenticationService.currentUserValue;
-  }
+  ) { }
 
   ngOnInit() {
-    this.loadAllUsers();
-  }
-
-  deleteUser(id: number) {
-    this.userService.delete(id)
-      .pipe(first())
-      .subscribe(() => this.loadAllUsers());
-  }
-
-  private loadAllUsers() {
-    this.userService.getAll()
-      .pipe(first())
-      .subscribe(users => this.users = users);
   }
 }
--- food-delivery-app/src/app/components/login/login.component.ts
+++ food-delivery-app/src/app/components/login/login.component.ts
@@ -5,7 +5,6 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/api/auth.service';
 import { StorageHelper } from 'src/app/core/helpers/storage.helper';
-import { AuthenticationService } from 'src/app/core/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -22,81 +21,76 @@ export class LoginComponent implements OnInit {
     private authService: AuthService,
     private _snackBar: MatSnackBar,
     private router: Router,
-    public dialog: MatDialog,
-    private authenticationService: AuthenticationService
-  ) { 
-    if (this.authenticationService.currentUserValue) {
-      this.router.navigate(['/browse']);
-  }
-  }
+    public dialog: MatDialog
+  ) {}
 
-  ngOnInit(): void {
-    this.initForm();
-  }
+ngOnInit(): void {
+  this.initForm();
+}
 
-  onHome() {
-    (<any>this.router).navigate(["/home"])
-  }
+onHome() {
+  (<any>this.router).navigate(["/home"])
+}
 
-  initForm() {
-    this.loginFormInstance = this.formBuilder.group({
-      email: ["", Validators.compose([
-        Validators.required,
-        Validators.email,
-        Validators.maxLength(30)
-      ])],
-      password: ["", Validators.compose([
-        Validators.required,
-        Validators.maxLength(30)
-      ])]
-    })
-  }
+initForm() {
+  this.loginFormInstance = this.formBuilder.group({
+    email: ["", Validators.compose([
+      Validators.required,
+      Validators.email,
+      Validators.maxLength(30)
+    ])],
+    password: ["", Validators.compose([
+      Validators.required,
+      Validators.maxLength(30)
+    ])]
+  })
+}
 
-  onLogin() {
-    if (this.loginFormInstance.invalid) {
-      return;
-    }
-
-    if (this.email != null && this.password != null)
-      this.authService.login({ email: this.email.value, password: this.password.value }).subscribe((response: any) => {
-        StorageHelper.setToken(response.token),
-          (<any>this.router).navigate(["/home"])
-      }, error => {
-        console.log(error)
-        console.log(error.error.error)
-        this.openSnackBar(error.error.error, "Close")
-      });
+onLogin() {
+  if (this.loginFormInstance.invalid) {
+    return;
   }
 
-  // openDialog() {
-  //   const dialogRef = this.dialog.open(RegisterFormComponent, {
-  //     width: '45vh',
-  //     height: '75vh'
-  //   });
-  //   dialogRef.afterClosed().subscribe(result => {
-  //     console.log(result);
-  //     if (result) {
-  //       let user = {
-  //         firstName: result.value.firstName,
-  //         lastName: result.value.lastName,
-  //         username: result.value.username,
-  //         email: result.value.email,
-  //         password: result.value.password
-  //       }
-  //     }
-  //   })
-  // }
+  if (this.email != null && this.password != null)
+    this.authService.login({ email: this.email.value, password: this.password.value }).subscribe((response: any) => {
+      StorageHelper.setToken(response.token),
+        (<any>this.router).navigate(["/home"])
+    }, error => {
+      console.log(error)
+      console.log(error.error.error)
+      this.openSnackBar(error.error.error, "Close")
+    });
+}
 
-  openSnackBar(message: string, action: string) {
-    this._snackBar.open(message, action);
-  }
+// openDialog() {
+//   const dialogRef = this.dialog.open(RegisterFormComponent, {
+//     width: '45vh',
+//     height: '75vh'
+//   });
+//   dialogRef.afterClosed().subscribe(result => {
+//     console.log(result);
+//     if (result) {
+//       let user = {
+//         firstName: result.value.firstName,
+//         lastName: result.value.lastName,
+//         username: result.value.username,
+//         email: result.value.email,
+//         password: result.value.password
+//       }
+//     }
+//   })
+// }
+
+openSnackBar(message: string, action: string) {
+  this._snackBar.open(message, action);
+}
 
   get email() {
-    return this.loginFormInstance.get("email");
-  }
+  return this.loginFormInstance.get("email");
+}
 
   get password() {
-    return this.loginFormInstance.get("password");
-  }
+  return this.loginFormInstance.get("password");
+}
 
 }
