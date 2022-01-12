165c4fd82fd6c02dea62ce36ba79bdce775abed3
GeorgeRosianu <georgerosianu12@gmail.com>
2022-01-12 17:03:15 +0200

    Added 85C Restaurant

--- food-delivery-app/src/app/components/restaurant1/restaurant1.component.ts
+++ food-delivery-app/src/app/components/restaurant1/restaurant1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-restaurant1',
@@ -7,9 +8,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Restaurant1Component implements OnInit {
 
-  constructor() { }
+  panelOpenState = false;
+  durationInSeconds = 2;
+
+  constructor(
+    private _snackBar: MatSnackBar,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  openSnackBar() {
+    this._snackBar.open('Item added to your cart!', '', {duration: this.durationInSeconds * 1000});
+  }
+
+  openSnackBar2() {
+    this._snackBar.open('Order Placed!', '', {duration: this.durationInSeconds * 1000});
+  }
+
+  onCreamPuffs() {
+    this.openSnackBar();
+  }
+
+  onEggTart() {
+    this.openSnackBar();
+  }
+
+  onBlackForest() {
+    this.openSnackBar();
+  }
+
+  onCheeseBites() {
+    this.openSnackBar();
+  }
+
+  onCoconutTart() {
+    this.openSnackBar();
+  }
+
+  onBerrytale() {
+    this.openSnackBar();
+  }
+
+  onBorohCream() {
+    this.openSnackBar();
+  }
+
+  onBoroh() {
+    this.openSnackBar();
+  }
+
+  onBrioche() {
+    this.openSnackBar();
+  }
+
+  onCheeseBread() {
+    this.openSnackBar();
+  }
+
+  onCoffee() {
+    this.openSnackBar();
+  }
+
+  onAmericano() {
+    this.openSnackBar();
+  }
+
+  onTea() {
+    this.openSnackBar();
+  }
+
+  onBoba() {
+    this.openSnackBar();
+  }
+
+  onLatte() {
+    this.openSnackBar();
+  }
+
+  onOrder() {
+    this.openSnackBar2();
+  }
 }
--- food-delivery-app/src/app/components/restaurant1/restaurant1.module.ts
+++ food-delivery-app/src/app/components/restaurant1/restaurant1.module.ts
@@ -4,6 +4,14 @@ import { CommonModule } from '@angular/common';
 import { Restaurant1RoutingModule } from './restaurant1-routing.module';
 import { Restaurant1Component } from './restaurant1.component';
 import { ToolbarModule } from '../toolbar/toolbar.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+
 
 @NgModule({
   declarations: [
@@ -12,6 +20,12 @@ import { ToolbarModule } from '../toolbar/toolbar.module';
   imports: [
     CommonModule,
     Restaurant1RoutingModule,
+    MatCardModule,
+    MatIconModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatListModule,
+    MatExpansionModule,
     ToolbarModule
   ]
 })
